497e9320e388df8906c97ef6263d9779210747fa
forhad <forhadmethun@gmail.com>
2022-07-21 17:49:56 +0300

    Reformat code for blog react-node application

--- learning/micro-service/blog/client/src/App.js
+++ learning/micro-service/blog/client/src/App.js
@@ -1,15 +1,16 @@
-import React from 'react'; 
-import PostCreate from './PostCreate';  
-import PostList from './PostList';
+import React from "react";
+import PostCreate from "./PostCreate";
+import PostList from "./PostList";
 
 const App = () => {
-    return <div className="container">
-        <h1>Create Post!!!</h1>
-        <PostCreate />
-        <hr/>
-        <PostList />
+  return (
+    <div className="container">
+      <h1>Create Post!!!</h1>
+      <PostCreate />
+      <hr />
+      <PostList />
     </div>
-}
-
+  );
+};
 
 export default App;
