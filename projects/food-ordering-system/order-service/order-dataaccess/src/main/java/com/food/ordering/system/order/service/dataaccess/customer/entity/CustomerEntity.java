c904ea9b14e6b05b94e16c0f3d7bca3213660169
forhad <forhadmethun@gmail.com>
2023-11-14 08:11:09 +0200

    Add customer data access <entity, mapper, repository, adapter>

---
+++ projects/food-ordering-system/order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/customer/entity/CustomerEntity.java
@@ -0,0 +1,23 @@
+package com.food.ordering.system.order.service.dataaccess.customer.entity;
+
+import java.util.UUID;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "order_customer_m_view", schema = "customer")
+@Entity
+public class CustomerEntity {
+  @Id
+  private UUID id;
+}
